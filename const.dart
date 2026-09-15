3566a8dd6a6de7e965666c1b6b3e7c0672aab403
rhkrrbfl09 <k94636596@gmail.com>
2026-09-15 20:51:51 +0900

    c

---
+++ const.dart
@@ -0,0 +1,7 @@
+const weekdays = ['월', '화', '수', '목', '금'];
+void main() {
+  print('요일: $weekdays');
+  print('개수: ${weekdays.length}');
+  print('첫 날: ${weekdays.first}');
+  print('마지막 날: ${weekdays.last}');
+}
---
+++ profile.dart
@@ -0,0 +1,11 @@
+const name = '김아린';
+const job = 'Dart 개발자';
+void main() {
+  const years = 3;
+  final headline = '$name / $job';
+  print('=== 프로필 ===');
+  print('이름: $name');
+  print('직업: $job');
+  print('경력: $years 년');
+  print('한 줄 소개: $headline');
+}
